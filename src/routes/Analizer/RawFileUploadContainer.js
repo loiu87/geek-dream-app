7d12c708ec07dddccc9e5d29c3521aae4bb95560
jwkim <jwkim@tonentones.com>
2020-12-18 01:29:53 +0900

    Fixed comma split problem with description contain commas

--- src/routes/Analizer/RawFileUploadContainer.js
+++ src/routes/Analizer/RawFileUploadContainer.js
@@ -5,17 +5,22 @@ import * as XLSX from "xlsx";
 import { DataContext } from "./RouteControler";
 
 const convertToJson = (csv) => {
-  var lines = csv.split("\n");
+  let lines = csv.split("\n");
   var result = [];
-
   var headers = lines[0].split(",");
 
   for (var i = 1; i < lines.length; i++) {
     var obj = {};
-    var currentline = lines[i].split(",");
+    let pattern = /".*?"/g;
+    let newLines = lines[i].replace(pattern, (data) => {
+      return data.replace(/,/g, "`");
+    });
+    var currentline = newLines.split(",");
 
     for (var j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j];
+      if (currentline[j] !== undefined) {
+        obj[headers[j]] = currentline[j].replace(/`/g, ",");
+      }
     }
 
     result.push(obj);
